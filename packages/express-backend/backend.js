3513f9aca8dc06e66a952edeb7ae540fa0b3572a
Scott Eisenberg <seisenberg2015@gmail.com>
2026-04-14 17:27:15 -0700

    added user retrieval via id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -13,6 +13,19 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
