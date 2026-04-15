7eb390b597e9c474fd8519ad252daa984ac448b4
Scott Eisenberg <seisenberg2015@gmail.com>
2026-04-14 17:42:01 -0700

    added extra tasks

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -63,11 +63,54 @@ const users = {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+  const job = req.query.job;
+
+  if (name !== undefined && job !== undefined) {
+    const result = findUsersByNameAndJob(name, job);
+    res.send({ users_list: result });
+  } else if (name !== undefined) {
+    const result = users.users_list.filter((user) => user.name === name);
+    res.send({ users_list: result });
   } else {
     res.send(users);
   }
 });
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex((user) => user.id === id);
+
+  if (index === -1) {
+    return false;
+  }
+
+  users.users_list.splice(index, 1);
+  return true;
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(204).send();
+  }
+});
+
+const findUsersByNameAndJob = (name, job) => {
+  return users.users_list.filter(
+    (user) => user.name === name && user.job === job
+  );
+};
